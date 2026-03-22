f41459f2b8a2eaf7c31522b6cd60e2ce1048600a
JunBin05 <limjunbin05@gmail.com>
2026-03-22 19:43:37 +0800

    Popup

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import { AuthProvider } from './context/AuthContext';
 import { CheckInListener } from './components/CheckInListener';
+import { WaitlistFulfillmentListener } from './components/WaitlistFulfillmentListener';
 
 export default function App() {
   return (
     <AuthProvider>
       <CheckInListener />
+      <WaitlistFulfillmentListener />
       <RouterProvider router={router} />
     </AuthProvider>
   );
